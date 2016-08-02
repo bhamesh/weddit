90b766165cd114d13aa500583ed713baa9ca1074
Bhamesh Kore <bhams91.kore@gmail.com>
2016-08-02 18:33:10 +0530

    [mod] added family member table

--- weddit/weddit/doctype/weddit_candidate/weddit_candidate.py
+++ weddit/weddit/doctype/weddit_candidate/weddit_candidate.py
@@ -9,5 +9,21 @@ from frappe.model.document import Document
 
 class WedditCandidate(Document):
 	def validate(self):
+		self.validate_mobile_no()
+		self.validate_family_members()
+
+	def validate_mobile_no(self):
 		if len(self.mobile_no) != 10:
 			frappe.throw(_("Mobile number invalid!"))
+
+	def validate_family_members(self):
+		member_list = []
+
+		for member in self.family_members:
+			if member.relation.lower() in ["mother", "father"]:
+				member_list.append(member.relation)
+
+		if len(member_list)!= len(set(member_list)):
+			frappe.throw(_("Father or Mother cannot repeat in relation"))
+		
+			
